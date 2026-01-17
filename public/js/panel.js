039d4bde14fe3ce0b3443bc7df7e4a3ad9e0d3f3
sgzti <asantyea@gmail.com>
2026-01-16 18:20:58 -0600

    panel admin avanze

--- public/js/panel.js
+++ public/js/panel.js
@@ -3,14 +3,15 @@ let sidebar = document.querySelectorAll("li")
 
 
 
-function limpiarActivo(){
+function limpiarActivo() {
     sidebar.forEach(elemento => {
-     elemento.classList.remove("activo")
+        elemento.classList.remove("activo")
 
-})}
+    })
+}
 
-function limpiarContent(){
-    content.forEach(seccion =>{
+function limpiarContent() {
+    content.forEach(seccion => {
         seccion.style.display = "none"
     })
 }
@@ -19,12 +20,34 @@ sidebar.forEach(elemento => {
     elemento.addEventListener('click', function (event) {
         limpiarActivo()
         limpiarContent()
-        let idDestino = elemento.getAttribute("data-seccion")
+        let idDestino = elemento.getAttribute("seccion-datos")
         let seccionAMostrar = document.getElementById(idDestino)
-            if (seccionAMostrar) {
+        if (seccionAMostrar) {
             seccionAMostrar.style.display = "block"
         }
-        
+
         elemento.classList.add("activo")
     })
 })
+
+document.querySelectorAll('.btn-position').forEach(btn => {
+    btn.addEventListener('mouseenter', e => {
+      const rect = btn.getBoundingClientRect()
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
+  
+      const span = btn.querySelector('span')
+      span.style.left = x + 'px'
+      span.style.top = y + 'px'
+    })
+  
+    btn.addEventListener('mouseleave', e => {
+      const rect = btn.getBoundingClientRect()
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
+  
+      const span = btn.querySelector('span')
+      span.style.left = x + 'px'
+      span.style.top = y + 'px'
+    })
+  })
